824d73b2d2787b9d7941654d6dbbba601657803f
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-05-03 09:28:33 -0700

    add home and app routes

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def home
+  end
+
+  def app
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+  get 'welcome/app', as: 'app'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
 end
