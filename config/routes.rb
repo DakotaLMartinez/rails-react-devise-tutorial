9f20d5c27e4feef537edc10d7905558d70a7f3a9
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-05-07 09:31:08 -0700

    add posts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
   get 'welcome/home'
   get '/app', to: 'welcome#app', as: 'app'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_170520) do
+ActiveRecord::Schema.define(version: 2019_05_07_162309) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
