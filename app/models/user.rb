6f2c6f0067171654c1944c147bba84ccd0b45eab
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-05-08 14:17:32 -0700

    add seed posts to first user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.first || User.create(email: 'test@test.com', password: 'password', password_confirmation: 'password')
+posts = [
+  {
+    title: 'My first post', 
+    content: 'The start of something special'
+  },
+  {
+    title: 'My second post', 
+    content: 'This is really getting good'
+  },
+  {
+    title: 'Oh my god, Yeah!!!',
+    content: 'Enough said.'
+  }
+]
+posts.each do |post_hash|
+  user.posts.create(post_hash)
+end
